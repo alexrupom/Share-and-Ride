28df4860acc04422a3c134a4931167528aabe507
shafin <oshafin@gmail.com>
2017-11-20 00:39:27 +0600

    login

--- Application/Ride&Share/app/src/main/java/shafin/rideshare/MainActivity.java
+++ Application/Ride&Share/app/src/main/java/shafin/rideshare/MainActivity.java
@@ -3,6 +3,7 @@ package shafin.rideshare;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,8 +21,11 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //actionBar.hide();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
